88afac98c420c56856092c751b37a5f743ba7240
Arem Boyarchuk <boyarchukartem@gmail.com>
2020-03-24 13:03:53 +0300

    2.5.2 completed

--- linux_kernel_modules/2.5.2/solution.c
+++ linux_kernel_modules/2.5.2/solution.c
@@ -5,14 +5,31 @@
 #include <linux/init.h>
 
 
+static int my_sys_counter = 0;
+
 static struct kobject *my_kobject;
 
-/*
- * An unnamed attribute group will put all of the attributes directly in
- * the kobject directory.  If we specify a name, a subdirectory will be
- * created for the attributes with the directory being the name of the
- * attribute group.
- */
+
+static ssize_t my_sys_show(struct kobject *kobj, struct kobj_attribute *atte, char *buf)
+{
+	my_sys_counter++;
+	return sprintf(buf, "%d\n", my_sys_counter);
+}
+
+static ssize_t my_sys_store(struct kobject *kobj, struct kobj_attribute *attr,
+			 const char *buf, size_t count)
+{
+	return count;
+}
+
+static struct kobj_attribute my_sys_attribute =
+	__ATTR(my_sys, 0664, my_sys_show, my_sys_store);
+
+static struct attribute *attrs[] = {
+	&my_sys_attribute.attr,
+	NULL,
+};
+
 static struct attribute_group attr_group = {
 	.attrs = attrs,
 };
@@ -21,14 +38,16 @@ static int __init init_sulution(void)
 {
     int retval;
 
-    my_kobject = kobject_create_and_add("kobject_example", kernel_kobj);
+    my_kobject = kobject_create_and_add("my_kobject", kernel_kobj);
 	if (!my_kobject)
 	{
 		retval = -ENOMEM;
 	}
 	else
 	{
-
+		retval = sysfs_create_group(my_kobject, &attr_group);
+		if (retval)
+			kobject_put(my_kobject);
 	}
 
 
