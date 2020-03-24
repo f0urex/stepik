68ae391beedd242746a92e7f58aa6f46b9cee099
Arem Boyarchuk <boyarchukartem@gmail.com>
2020-03-24 11:55:07 +0300

    daily saving

---
+++ linux_kernel_modules/2.5.2/.solution.ko.cmd
@@ -0,0 +1,1 @@
+cmd_/home/artem/projects/stepik/linux_kernel_modules/2.5.2/solution.ko := ld -r -m elf_x86_64 -z max-page-size=0x200000 -T ./scripts/module-common.lds --build-id  -o /home/artem/projects/stepik/linux_kernel_modules/2.5.2/solution.ko /home/artem/projects/stepik/linux_kernel_modules/2.5.2/solution.o /home/artem/projects/stepik/linux_kernel_modules/2.5.2/solution.mod.o ;  true
--- linux_kernel_modules/2.5.2/solution.c
+++ linux_kernel_modules/2.5.2/solution.c
@@ -1,15 +1,43 @@
+#include <linux/kobject.h>
+#include <linux/string.h>
+#include <linux/sysfs.h>
 #include <linux/module.h>
-#include <linux/slab.h>
-#include <linux/device.h>
+#include <linux/init.h>
+
+
+static struct kobject *my_kobject;
+
+/*
+ * An unnamed attribute group will put all of the attributes directly in
+ * the kobject directory.  If we specify a name, a subdirectory will be
+ * created for the attributes with the directory being the name of the
+ * attribute group.
+ */
+static struct attribute_group attr_group = {
+	.attrs = attrs,
+};
 
 static int __init init_sulution(void)
 {
-    return 0;
+    int retval;
+
+    my_kobject = kobject_create_and_add("kobject_example", kernel_kobj);
+	if (!my_kobject)
+	{
+		retval = -ENOMEM;
+	}
+	else
+	{
+
+	}
+
+
+    return retval;
 }
 
 static void __exit cleanup_sulution(void)
 {
-
+	kobject_put(my_kobject);
 }
 
 module_init(init_sulution);
